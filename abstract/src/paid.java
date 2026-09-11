b55fb6e167f50398cddc51f6f49e4a50fc60529c
Sannihitha-369 <bondhalasannihitha@gmail.com>
2026-09-11 19:33:29 +0530

    Add PaidMain class with payment method examples

---
+++ abstract/src/paid.java
@@ -0,0 +1,13 @@
+package Com.Interfaces;
+
+public class PaidMain {
+    public static void main(String[] args) {
+        UPI upi = new UPI();
+        CreditCard cc = new CreditCard();
+        Cash c = new Cash();
+
+        upi.payAmount(500);
+        cc.payAmount(2300.02);
+        c.payAmount(1230);
+    }
+}
