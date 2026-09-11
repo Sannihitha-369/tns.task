244ccf7b6caa1cd195d0f22066a3c93f4203ce23
Sannihitha-369 <bondhalasannihitha@gmail.com>
2026-09-11 19:43:18 +0530

    Delete abstract/src directory

--- abstract/src/account.java
+++
@@ -1,6 +0,0 @@
-package Com.Interfaces;
-
-public interface Account {
-    String name = "Sannihitha"; // by default public static final
-    public void payAmount(double amount); // by default public abstract
-}
--- abstract/src/cash.java
+++
@@ -1,9 +0,0 @@
-package Com.Interfaces;
-
-public class Cash implements Account{
-    @Override
-    public void payAmount(double amount) {
-        System.out.println(name+" successfully paid by Cash");
-        System.out.println("The amount is: "+amount);
-    }
-}
