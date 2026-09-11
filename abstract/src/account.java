6779574f2f74aaa3075e6f7a8e5bc91773a46dd1
Sannihitha-369 <bondhalasannihitha@gmail.com>
2026-09-11 19:31:43 +0530

    Create Account interface with payAmount method

---
+++ abstract/src/account.java
@@ -0,0 +1,6 @@
+package Com.Interfaces;
+
+public interface Account {
+    String name = "Sannihitha"; // by default public static final
+    public void payAmount(double amount); // by default public abstract
+}
